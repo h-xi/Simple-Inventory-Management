cba3e02a18744fb30e0918b52c4907f191fa0024
Has Xi <aichex@students.cs.ubc.ca>
2022-07-31 16:45:56 -0700

    working async/await version of createTable

--- server/utils/controllers/productController.js
+++ server/utils/controllers/productController.js
@@ -1,5 +1,19 @@
 const pool = require("../DBConnector");
 
+const promisePool = pool.promise();
+
+const createTest = async () => {
+  try {
+    const [rows, fields] = await promisePool.query(
+      "CREATE TABLE hello (name VARCHAR(10))"
+    );
+    console.log(rows);
+    console.log(fields);
+  } catch (err) {
+    throw err;
+  }
+};
+
 const createSupplier = () => {
   let sql =
     "CREATE TABLE supplier (s_contact VARCHAR(50), supplier_name VARCHAR(40), supplier_ID INT)";
@@ -10,4 +24,13 @@ const createSupplier = () => {
   });
 };
 
-createProduct();
+createTest();
+
+// const createTable = (tableName, params) => {
+//   let sql = `CREATE TABLE ${tableName} ${params}`;
+//   pool.query(sql, function (err, res) {
+//     if (err) throw err;
+//     console.log("i am here");
+//     return res;
+//   });
+// };
