d871995acc9f33a684f1e2e4a09ec82c638abf12
Has Xi <aichex@students.cs.ubc.ca>
2022-07-31 14:03:10 -0700

    Set up Server + Client codebase

---
+++ server/db_index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mySQL = require("mysql");
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
+
+const HOST = process.env.HOST;
+const MYUSER = process.env.MYUSER;
+const PASSWORD = process.env.PASSWORD;
+const DATABASE = process.env.DATABASE;
+
+const myServer = express();
+
+const database = mySQL.createConnection({
+  host: HOST,
+  user: MYUSER,
+  password: PASSWORD,
+  database: DATABASE,
+  multipleStatements: true,
+});
+
+database.connect(function (error) {
+  if (error) {
+    console.error("error connecting :" + error.stack);
+    return;
+  }
+  console.log("Connected as id :" + database.threadId);
+});
+
+myServer.use(bodyParser.json());
+myServer.use(bodyParser.urlencoded({ extended: true }));
+
+myServer.listen(5000);
