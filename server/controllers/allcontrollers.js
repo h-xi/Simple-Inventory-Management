74757da083418ef9db75efd4f62ebc4d864affbb
jordanjzhao <zhao.jordan@hotmail.com>
2022-07-31 17:42:58 -0700

    adding controllers

--- server/controllers/allcontrollers.js
+++ server/controllers/allcontrollers.js
@@ -1,4 +1,8 @@
-const createBrand = async () => {
+const pool = require("../dbConnector.js");
+
+const promisePool = pool.promise();
+
+const createBrands = async () => {
     try {
       const [rows, fields] = await promisePool.query(
         "CREATE TABLE Brands(Brand_ID Integer, B_name Char(30), PRIMARY KEY (Brand_ID))"
@@ -8,4 +12,54 @@ const createBrand = async () => {
     } catch (err) {
       throw err;
     }
-  };
+};
+
+createBrands();
+
+const CreateAisle = async () => {
+    try {
+      const [rows, fields] = await promisePool.query(
+        "CREATE TABLE Aisle(Aisle_no Integer, PRIMARY KEY (Aisle_no))"
+      );
+      console.log(rows);
+      console.log(fields);
+    } catch (err) {
+      throw err;
+    }
+};
+
+CreateAisle();
+
+const CreateBin = async () => {
+    try {
+      const [rows, fields] = await promisePool.query(
+        "CREATE TABLE Bin(Bin_name CHAR(2), Capacity Integer, Aisle_no Integer, quant_filled Integer, PRIMARY KEY (Bin_name,Aisle_no), FOREIGN KEY (Aisle_no) REFERENCES Aisle ON DELETE NO ACTION)"
+      );
+      console.log(rows);
+      console.log(fields);
+    } catch (err) {
+      throw err;
+    }
+};
+
+CreateBin();
+
+const CreateCategories = async () => {
+    try {
+      const [rows, fields] = await promisePool.query(
+        "CREATE TABLE Categories(Category_ID Integer, Cat_name Char(30), Aisle_no Integer, PRIMARY KEY (Category_ID), FOREIGN KEY (Aisle_no) REFERENCES Aisle)"
+      );
+      console.log(rows);
+      console.log(fields);
+    } catch (err) {
+      throw err;
+    }
+};
+
+CreateCategories();
+
+    
+
+    
+
+  
