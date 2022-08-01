7ca22bacb49260b509c4b761e9c088d8cd13d0ba
Has Xi <aichex@students.cs.ubc.ca>
2022-07-31 17:59:22 -0700

    productRouter

---
+++ server/routes/productRouter.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { createProduct } = require("../controllers/allcontrollers");
+
+router.post("/", async () => {
+  let errors = [];
+});
