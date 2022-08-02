26d146f885a7cca843d8cd2322d632efca742ea2
jordanjzhao <zhao.jordan@hotmail.com>
2022-08-01 22:43:27 -0700

    Changes to employeeController

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -17,8 +17,24 @@ const getManager = async (employee_id) => {
 
 getManager(12345);
 
-const getEmployee = async (employee_id) => {
-  let sql = `SELECT * FROM Employee WHERE id = '${employee_id}';`;
+const getWorker = async (employee_id) => {
+  let sql = `SELECT * FROM WorkerEmployee WHERE Employee_ID = '${employee_id}';`;
+  try {
+    let query = await promisePool.query(sql);
+    query = JSON.parse(JSON.stringify(query));
+    console.log(query);
+    if (query) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (e) {
+    throw e;
+  }
+};
+
+const getDriver = async (employee_id) => {
+  let sql = `SELECT * FROM DriverEmployee WHERE Employee_ID = '${employee_id}';`;
   try {
     let query = await promisePool.query(sql);
     query = JSON.parse(JSON.stringify(query));
