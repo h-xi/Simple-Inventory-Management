6c4bfcfb3889f87a01bf56d2e5a4fa11a2e0e425
jordanjzhao <zhao.jordan@hotmail.com>
2022-08-02 14:54:23 -0700

    added useStates

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,35 +1,92 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [orderId, setOrderId] = useState(0);
+  const [itemNumber, setItemNumber] = useState(0);
+  const [shipmentDate, setShipmentDate] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+  const [assignedReceiver, setAssignedReceiver] = useState("");
+  const [assignedDriver, setAssignedDriver] = useState("");
+  const [deliveryAddress, setDeliveryAddress] = useState("");
+
+  const displayInfo = () => {
+    console.log(orderId + itemNumber + shipmentDate + quantity + assignedReceiver + assignedDriver + deliveryAddress);
+  };
+
   return (
     <div className="App">
       <div className="Incoming">
         <label>Order ID:</label>
-        <input type="number" />
+        <input type="number" 
+          onChange={(event) => {
+            setOrderId(event.target.value);
+          }}
+        />
         <label>Item Number:</label>
-        <input type="number" />
+        <input type="number" 
+          onChange={(event) => {
+            setItemNumber(event.target.value);
+          }}
+        />
         <label>Shipment Date:</label>
-        <input type="date" />
+        <input type="date" 
+          onChange={(event) => {
+            setShipmentDate(event.target.value);
+          }}
+        />
         <label>Quantity:</label>
-        <input type="number" />
+        <input type="number" 
+          onChange={(event) => {
+            setQuantity(event.target.value);
+          }}
+        />
         <label>Assigned Receiver:</label>
-        <input type="text" />
-        <button>Incoming Shipment</button>
+        <input type="text" 
+          onChange={(event) => {
+            setAssignedDriver(event.target.value);
+          }}
+        />
+        <button onClick={displayInfo}>Incoming Shipment</button>
       </div>
       <div className="Outgoing">
         <label>Order ID:</label>
-        <input type="number" />
+        <input type="number" 
+          onChange={(event) => {
+            setOrderId(event.target.value);
+          }}
+        />
         <label>Item Number:</label>
-        <input type="number" />
+        <input type="number" 
+          onChange={(event) => {
+            setItemNumber(event.target.value);
+          }}
+        />
         <label>Shipment Date:</label>
-        <input type="date" />
+        <input type="date" 
+          onChange={(event) => {
+            setShipmentDate(event.target.value);
+          }}
+        />
         <label>Quantity:</label>
-        <input type="number" />
+        <input type="number" 
+          onChange={(event) => {
+            setQuantity(event.target.value);
+          }}
+        />
         <label>Assigned Driver:</label>
-        <input type="text" />
+        <input type="text" 
+          onChange={(event) => {
+            setAssignedDriver(event.target.value);
+          }}
+        />
         <label>Delivery Address:</label>
-        <input type="text" />
-        <button>Outgoing Shipment</button>
+        <input type="text" 
+          onChange={(event) => {
+            setDeliveryAddress(event.target.value);
+          }}
+        />
+        <button onClick={displayInfo}>Outgoing Shipment</button>
       </div>
     </div>
   );
