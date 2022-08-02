991191a20ac84e91c5ff1f41a57bbf9405292ce0
Has Xi <aichex@students.cs.ubc.ca>
2022-08-02 00:57:47 -0700

    working addOrder endpoint!!! :D

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -2,51 +2,20 @@ const pool = require("../dbConnector.js");
 
 const promisePool = pool.promise();
 
-const getManager = async (employee_id) => {
-  let sql = `SELECT * FROM ManagerEmployee WHERE Employee_ID = ${employee_id}`;
-
+const getEmployee = async (table, employee_id) => {
+  let sql = `SELECT * FROM ${table} WHERE Employee_ID = '${employee_id}';`;
   try {
     let query = await promisePool.query(sql);
     query = JSON.parse(JSON.stringify(query));
-    console.log(query);
+    const result = query[0][0];
+    console.log(result);
+    return result;
   } catch (e) {
     console.error(e);
     throw e;
   }
 };
 
-getManager(12345);
-
-const getWorker = async (employee_id) => {
-  let sql = `SELECT * FROM WorkerEmployee WHERE Employee_ID = '${employee_id}';`;
-  try {
-    let query = await promisePool.query(sql);
-    query = JSON.parse(JSON.stringify(query));
-    console.log(query);
-    if (query) {
-      return true;
-    } else {
-      return false;
-    }
-  } catch (e) {
-    throw e;
-  }
+module.exports = {
+  getEmployee: getEmployee,
 };
-
-const getDriver = async (employee_id) => {
-  let sql = `SELECT * FROM DriverEmployee WHERE Employee_ID = '${employee_id}';`;
-  try {
-    let query = await promisePool.query(sql);
-    query = JSON.parse(JSON.stringify(query));
-    console.log(query);
-    if (query) {
-      return true;
-    } else {
-      return false;
-    }
-  } catch (e) {
-    throw e;
-  }
-};
-
-module.exports = { getEmployee: getEmployee };
