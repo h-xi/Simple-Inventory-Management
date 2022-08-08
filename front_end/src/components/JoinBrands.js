5e082ea34c9fcff5aaa6bce16b21df79093ebae8
Jordan Zhao <zhao.jordan@hotmail.com>
2022-08-07 20:07:20 -0700

    front_end for all minus join, need to display data

--- front_end/src/components/JoinBrands.js
+++ front_end/src/components/JoinBrands.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useState } from 'react';
+import Axios from 'axios';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 const JoinBrands = () => {
+
+  const [value, setValue] = useState(0);
+
+  const joinBrands = () => {
+      console.log({value})
+      Axios.get('http://localhost:3000/brands/', {params: {value}}
+    ).then(res => {
+      console.log(res.data);
+    }).catch(error => {
+      console.log(error.response.data);
+    });
+  }
   return (
-    <div>JoinBrands</div>
+    <div>
+      <label>Enter Value:</label>
+            <input type="number" 
+              onChange={(event) => {
+                setValue(event.target.value);
+              }}
+            />
+      <Stack direction="row" spacing={2}>
+              <Button variant="contained" onClick={joinBrands}>Select Attributes</Button>
+      </Stack>
+    </div>
   )
 }
 
