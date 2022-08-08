1ad216774b8a8513db8c0d7dc36a4aded19227cd
Jordan Zhao <zhao.jordan@hotmail.com>
2022-08-07 19:45:48 -0700

    groupaggregation button click, need to display

--- front_end/src/components/GroupAggregation.js
+++ front_end/src/components/GroupAggregation.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Axios from 'axios';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 const GroupAggregation = () => {
+
+  const groupAggregation = () => {
+
+      Axios.get('http://localhost:3000/products/aggregations/group/', 
+    ).then(res => {
+      console.log(res.data);
+    }).catch(error => {
+      console.log(error.response.data);
+    });
+  }
   return (
-    <div>GroupAggregation</div>
+    <div>
+      
+      <Stack direction="row" spacing={2}>
+              <Button variant="contained" onClick={groupAggregation}>Select Attributes</Button>
+      </Stack>
+    </div>
   )
 }
 
-export default GroupAggregation
+export default GroupAggregation;
