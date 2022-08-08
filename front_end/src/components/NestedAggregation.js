f23259a06076271f89bb70b55ded5550f9adb65c
Jordan Zhao <zhao.jordan@hotmail.com>
2022-08-07 19:50:59 -0700

    nested under join button click, need to display

--- front_end/src/components/NestedAggregation.js
+++ front_end/src/components/NestedAggregation.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Axios from 'axios';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 const NestedAggregation = () => {
+
+  const nestedAggregation = () => {
+
+      Axios.get('http://localhost:3000/products/aggregations/join/', 
+    ).then(res => {
+      console.log(res.data);
+    }).catch(error => {
+      console.log(error.response.data);
+    });
+  }
   return (
-    <div>NestedAggregation</div>
+    <div>
+      
+      <Stack direction="row" spacing={2}>
+              <Button variant="contained" onClick={nestedAggregation}>Select Attributes</Button>
+      </Stack>
+    </div>
   )
 }
 
-export default NestedAggregation
+export default NestedAggregation;
