afd1781c41d339e42d05b1836b659918fec70ca8
Jordan Zhao <zhao.jordan@hotmail.com>
2022-08-07 19:46:57 -0700

    havingagg button click, need to display data

--- front_end/src/components/HavingAggregation.js
+++ front_end/src/components/HavingAggregation.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Axios from 'axios';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 const HavingAggregation = () => {
+
+  const havingAggregation = () => {
+
+      Axios.get('http://localhost:3000/products/aggregations/having/', 
+    ).then(res => {
+      console.log(res.data);
+    }).catch(error => {
+      console.log(error.response.data);
+    });
+  }
   return (
-    <div>HavingAggregation</div>
-  );
+    <div>
+      
+      <Stack direction="row" spacing={2}>
+              <Button variant="contained" onClick={havingAggregation}>Select Attributes</Button>
+      </Stack>
+    </div>
+  )
 }
 
 export default HavingAggregation;
