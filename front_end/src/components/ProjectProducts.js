d9786a25d22a6db0cbfb5f4436e661476e51e143
Has Xi <aichex@students.cs.ubc.a>
2022-08-08 20:14:35 -0700

    code cleanup

--- front_end/src/components/ProjectProducts.js
+++ front_end/src/components/ProjectProducts.js
@@ -20,37 +20,10 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-// function createData(
-//   name: string,
-//   calories: number,
-//   fat: number,
-//   carbs: number,
-//   protein: number
-// ) {
-//   return { name, calories, fat, carbs, protein };
-// }
-
-// const rows = [
-//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-//   createData("Eclair", 262, 16.0, 24, 6.0),
-//   createData("Cupcake", 305, 3.7, 67, 4.3),
-//   createData("Gingerbread", 356, 16.0, 49, 3.9),
-// ];
-
 const ProjectProducts = () => {
-  // const [Barcode, setBarcode] = useState(0);
-  // const [P_name, setP_name] = useState("");
-  // const [Size, setSize] = useState("");
-  // const [Bin_name, setBin_name] = useState("");
-  // const [Aisle_no, setAisle_no] = useState(0);
-  // const [Category_ID, setCategory_ID] = useState(0);
-  // const [Brand_ID, setBrand_ID] = useState(0);
-  // const [Inventory_Barcode, setInventory_Barcode] = useState(0);
-  // const [SupplierID, setSupplierID] = useState(0);
-  // const [Price, setPrice] = useState(0);
   const [params, setParams] = useState([]);
   const [data, setData] = useState([]);
+
   const handleParams = (event) => {
     const index = params.indexOf(event.target.value);
     if (index === -1) {
@@ -58,17 +31,6 @@ const ProjectProducts = () => {
     } else {
       setParams(params.filter((param) => param !== event.target.value));
     }
-    // const index = (id) => {
-    //     setParams(prevState => {
-    //         if(prevState.includes(id)) {
-    //             const clone = [...prevState];
-    //             clone.splice(prevState.indexOf(id), 1)
-    //             return clone;
-    //         } else {
-    //             return [...prevState, id]
-    //         }
-    //     });
-    // }
   };
 
   const updateData = (initialData) => {
@@ -81,18 +43,6 @@ const ProjectProducts = () => {
     console.log(data);
     setData(data);
   };
-  // const incomingData = {
-  //     Barcode: Barcode,
-  //     P_name: P_name,
-  //     Size: Size,
-  //     Bin_name: Bin_name,
-  //     Aisle_no: Aisle_no,
-  //     Category_ID: Category_ID,
-  //     Brand_ID: Brand_ID,
-  //     Inventory_Barcode: Inventory_Barcode,
-  //     SupplierID: SupplierID,
-  //     Price: Price,
-  //   };
 
   const projectProducts = () => {
     Axios.post("http://localhost:3000/products/filtered/", { params: params })
@@ -245,22 +195,6 @@ const ProjectProducts = () => {
               </TableRow>
             ))}
           </TableBody>
-          {/* <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody> */}
         </Table>
       </TableContainer>
     </div>
