55dc4cddb6556a921026cc7332101e8d58fd0ff8
jordanjzhao <zhao.jordan@hotmail.com>
2022-08-08 18:46:37 -0700

    join so far

--- front_end/src/components/JoinBrands.js
+++ front_end/src/components/JoinBrands.js
@@ -3,11 +3,29 @@ import { useState } from "react";
 import Axios from "axios";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const JoinBrands = () => {
   const [numberTimes, setNumberTimes] = useState(0);
   const [data, setData] = useState([]);
   const result = []
+
+  const updateData = (initialData) => {
+    console.log(initialData);
+    let newData = [];
+    for (const items of initialData) {
+      let result = Object.values(items);
+      newData.push(result);
+    }
+    console.log(newData);
+    setData(newData);
+  };
 
 
   const joinBrands = () => {
@@ -17,16 +35,16 @@ const JoinBrands = () => {
     Axios.post("http://localhost:3000/brands/", { numberTimes: numberTimes })
       .then((res) => {
         console.log(res.data);
-        //
+        updateData(res.data);
       })
       .catch((error) => {
         console.log(error.response.data);
       });
   };
-
 //Find brands that have been ordered (outgoing shipment order) more than 'x' number of times
   return (
     <div>
+      
       <h3>Find brands that have been ordered in the outgoing shipment more than inputted number of times</h3>
       <label>Enter Number of Times:</label>
       <input
@@ -40,7 +58,7 @@ const JoinBrands = () => {
           Find Brands
         </Button>
       </Stack>
-      <table>
+      {/* <table>
         <tr>
           <th>Brand ID  </th>
           <th>Brand Name  </th>
@@ -57,7 +75,50 @@ const JoinBrands = () => {
         </tbody>
         
        
-        </table>
+        </table> */}
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <th>Brand ID  </th>
+            <th>Brand Name  </th>
+          </TableHead>
+          <TableBody>
+            {/* {data.map((datapoint) => (
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                {datapoint.map((point) => (
+                  <TableCell component="th" scope="row">
+                    {point}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))} */}
+            {data.map((d) => (
+            <tr>
+              <td>{d.Brand_ID}</td>
+              <td>{d.b_Name}</td>         
+            </tr>
+          ))}
+          </TableBody>
+          {/* <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody> */}
+        </Table>
+      </TableContainer>
     </div>
   );
 };
